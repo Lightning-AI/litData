ecf872e3883500c0a0c9f9f502a9ba88a8bf50e6
Deependu Jha <deependujha21@gmail.com>
2025-04-21 16:36:59 +0530

    update

--- .github/benchmark/benchmark.py
+++ .github/benchmark/benchmark.py
@@ -1,4 +1,4 @@
-"""This script is used to benchmark LitData in CI."""
+"""This script is used to benchmark LitData when invoked from a GitHub PR's comment."""
 
 import argparse
 from dataclasses import dataclass
