4b50cdda5b9f8d2295f4f0ff7561b04c9b9ebb15
Deependu Jha <deependujha21@gmail.com>
2025-05-14 21:22:35 +0530

    update

--- tests/processing/test_item_provider.py
+++ tests/processing/test_item_provider.py
@@ -4,19 +4,19 @@ from litdata.processing.item_provider import WorkerItemProvider  # replace with 
 
 
 def test_set_and_get_items_int_index():
-    provider = WorkerItemProvider([[[1], [2]], [[3], [4]]], num_downloaders=2)
-    provider.set_items(0, [[10], [20]])
-    assert provider.get_items(0) == [[10], [20]]
+    provider = WorkerItemProvider([[1, 2], [3, 4]], num_downloaders=2)
+    provider.set_items(0, [10, 20])
+    assert provider.get_items(0) == [10, 20]
 
 
 def test_set_and_get_items_tuple_index():
-    provider = WorkerItemProvider([[[1], [2]], [[3], [4]]], num_downloaders=2)
+    provider = WorkerItemProvider([[1, 2], [3, 4]], num_downloaders=2)
     provider.set_items((1, 0), [30])
     assert provider.get_items((1, 0)) == [30]
 
 
 def test_invalid_index_type():
-    provider = WorkerItemProvider([[[1], [2]]], num_downloaders=1)
+    provider = WorkerItemProvider([[1, 2], [3, 4]], num_downloaders=1)
     with pytest.raises(ValueError, match="Invalid index type"):
         provider.set_items("invalid", [0])
     with pytest.raises(ValueError, match="Invalid index type"):
@@ -25,7 +25,7 @@ def test_invalid_index_type():
 
 @pytest.mark.parametrize("use_shared", [True, False])
 def test_prepare_ready_to_use_queue(use_shared):
-    provider = WorkerItemProvider([[[1], [2], [3]], [[4], [5], [6]]], num_downloaders=2)
+    provider = WorkerItemProvider([[1, 2, 3], [4, 5, 6]], num_downloaders=2)
     provider.prepare_ready_to_use_queue(use_shared, worker_index=0)
 
     queue = provider.ready_to_process_shared_queue if use_shared else provider.ready_to_process_item[0]
