0d5a8ecbde11ecbfb0c9a2a071c58e2dfb3fb9a3
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2025-07-25 04:32:40 +0000

    fix(StreamingRawDataset): simplify return statement in __getitem__ method

--- src/litdata/streaming/raw_dataset.py
+++ src/litdata/streaming/raw_dataset.py
@@ -332,9 +332,7 @@ class StreamingRawDataset(Dataset):
         file_path = self.files[index].path
         loop = asyncio.get_event_loop()
         data = loop.run_until_complete(self.cache_manager.download_file_async(file_path))
-        if self.transform:
-            return self.transform(data)
-        return data
+        return self.transform(data) if self.transform else data
 
     def __getitems__(self, indices: list[int]) -> list[Any]:
         """Get multiple items efficiently using async batch download."""
